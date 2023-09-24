6811b171b06744dcfac9600609bf5c3326d78e78
Michael Chapman <m5l.c5n@gmail.com>
2023-09-24 19:58:45 -0400

    add basic functionality and style

--- main.js
+++ main.js
@@ -1,24 +1,68 @@
+import 'normalize.css/normalize.css'
 import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+
+import { streetDictionary } from './streets'
 
 document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
+  <div class="container">
+    <div class="stack">
+      <div class="label">
+        Street Name
+      </div>
+      <div class="street">
+        <input type="text" class="text_input" id="street_input" list="street_names">
+        <datalist id="street_names"></datalist>
+        </div>
+      <div id="district">
+        <div id="district_name">district</div>
+        <div id="district_location">location</div>
+        <div id="district_address">address</div>
+      </div>
     </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
   </div>
 `
+let datalist = document.querySelector('#street_names')
 
-setupCounter(document.querySelector('#counter'))
+Object.keys(streetDictionary).forEach((key) => {
+  let option = document.createElement('option')
+  option.value = key
+  datalist.appendChild(option)
+})
+
+const districts = {
+  1: {
+    location: 'West Hill School',
+    address: 'Cronin Drive'
+  },
+  2: {
+    location: 'Moser School',
+    address: '10 School Street'
+  },
+  3: {
+    location: 'Griswold Middle School',
+    address: '144 Bailey Road'
+  }
+}
+
+let streetInput = document.querySelector('#street_input')
+
+let district = document.querySelector('#district')
+
+let districtName = document.querySelector('#district_name')
+let districtLocation = document.querySelector('#district_location')
+let districtAddress = document.querySelector('#district_address')
+
+function onInput() {
+  const key = streetInput.value
+  if (key in streetDictionary) {
+    const value = streetDictionary[key]
+    districtName.textContent = `District ${value}`
+    districtLocation.textContent = districts[value].location
+    districtAddress.textContent = districts[value].address
+    district.setAttribute('style', 'display: flex;')
+  } else {
+    district.setAttribute('style', 'display: none;')
+  }
+}
+
+streetInput.addEventListener('input', onInput)
